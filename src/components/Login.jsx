c27b01705f1582b1c8839042671957bd36693024
riyandidjohari8 <riyandidjohari8@gmail.com>
2024-08-28 18:01:08 +0000

    feat(login): redirect to homepage if user already login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,17 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
+  const token = localStorage.getItem("token");
+
+  useEffect(() => {
+    if(token){
+      navigate("/");
+    }
+  }, [])
 
   const handleSubmit = (event) => {
     event.preventDefault();
